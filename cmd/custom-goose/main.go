d61a9fec8b540e3d4327af383dde552291173978
Mike Fridman <mf192@icloud.com>
2023-11-09 09:46:38 -0500

    update example with goose.Provider

--- cmd/custom-goose/main.go
+++ cmd/custom-goose/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/mfridman/goose-demo/migrations"
 	"github.com/pressly/goose/v3"
+	"github.com/pressly/goose/v3/database"
 	_ "modernc.org/sqlite"
 )
 
 func main() {
 	log.SetFlags(0)
+	ctx := context.Background()
 
 	tmp := os.TempDir()
 	defer os.RemoveAll(tmp)
@@ -19,13 +24,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	goose.SetDialect("sqlite3")
-	goose.SetBaseFS(migrations.Embed)
 
-	if err := goose.Up(db, "."); err != nil {
-		panic(err)
-	}
-	if err := goose.Version(db, "."); err != nil {
+	provider, err := goose.NewProvider(database.DialectSQLite3, db, migrations.Embed)
+	if err != nil {
 		log.Fatal(err)
+	}
+	// List migration sources the provider is aware of.
+	fmt.Println("\n=== migration list ===")
+	sources := provider.ListSources()
+	for _, s := range sources {
+		log.Printf("%-3s %-2v %v\n", s.Type, s.Version, filepath.Base(s.Path))
+		// sql 1  00001_users_table.sql
+		// go  2  00002_add_users.go
+		// go  3  00003_count_users.go
+	}
+
+	// List status of migrations before applying them.
+	stats, err := provider.Status(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("\n=== migration status ===")
+	for _, s := range stats {
+		fmt.Printf("%-3s %-2v %v\n", s.Source.Type, s.Source.Version, s.State)
+	}
+
+	fmt.Println("\n=== run migrations  ===")
+	results, err := provider.Up(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("\n=== migration results  ===")
+	for _, r := range results {
+		fmt.Printf("%-3s %-2v %v\n", r.Source.Type, r.Source.Version, r.Duration)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,23 +4,26 @@ go 1.20
 
 require (
 	github.com/moby/moby v24.0.6+incompatible
-	github.com/pressly/goose/v3 v3.15.1
-	modernc.org/sqlite v1.26.0
+	github.com/pressly/goose/v3 v3.15.2-0.20231109144512-3fade11d08db
+	modernc.org/sqlite v1.27.0
 )
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/google/uuid v1.3.1 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	golang.org/x/mod v0.12.0 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
 	lukechampine.com/uint128 v1.3.0 // indirect
 	modernc.org/cc/v3 v3.41.0 // indirect
 	modernc.org/ccgo/v3 v3.16.15 // indirect
-	modernc.org/libc v1.24.1 // indirect
+	modernc.org/libc v1.29.0 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.7.2 // indirect
 	modernc.org/opt v0.1.3 // indirect
